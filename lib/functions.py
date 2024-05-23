b2112f06dabd19eec6a64fb6fbb3c38e68fc5063
kenkipkoec <itsmetrokenaki@gmal.com>
2024-05-23 09:12:41 +0300

    Assignment completed

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,26 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Engineer!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
-def add(num1, num2):
-    pass
+greet_with_default()
+greet_with_default("Kipkoech!")
 
-def halve(number):
+
+def add(num1=2, num2=2):
     pass
+    return num1 + num2
+
+def halve(number=10):
+    pass
+    return number / 2
